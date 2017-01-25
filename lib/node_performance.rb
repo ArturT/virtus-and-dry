17076d803b76f795e90a607c33528fcdcda984fc
ArturT <arturtrzop@gmail.com>
2017-01-25 18:02:50 +0100

    Test for nodes with many items initialised by hash

--- lib/node_performance.rb
+++ lib/node_performance.rb
@@ -8,18 +8,33 @@ time_me = ->(proc) {
   puts time_in_millis
 }
 
+NODE_CREATE_LIMIT = 10_000
+HASH_ITEMS_LIMIT = 100
+NODE_WITH_MANY_ITEMS_CREATE_LIMIT = 100
+
+
+hash_items = []
+HASH_ITEMS_LIMIT.times { hash_items << {name: 'A', value: 1.1} }
+
 
 puts 'Init NodeWithVirtusValueObject with hash as items'
 time_me.call -> {
-  10000.times {
+  NODE_CREATE_LIMIT.times {
     NodeWithVirtusValueObject.new(name: 'Name', items: [{name: 'A', value: 1.1}])
   }
 }
 
 puts 'Init NodeWithVirtusValueObject with VirtusItem object as items'
 time_me.call -> {
-  10000.times {
+  NODE_CREATE_LIMIT.times {
     NodeWithVirtusValueObject.new(name: 'Name', items: [VirtusItem.new(name: 'A', value: 1.1)])
+  }
+}
+
+puts 'Init NodeWithVirtusValueObject with many hash as items'
+time_me.call -> {
+  NODE_WITH_MANY_ITEMS_CREATE_LIMIT.times {
+    NodeWithVirtusValueObject.new(name: 'Name', items: hash_items)
   }
 }
 
@@ -27,14 +42,21 @@ puts '-'*20
 
 puts 'Init NodeWithDryStruct with hash as items'
 time_me.call -> {
-  10000.times {
+  NODE_CREATE_LIMIT.times {
     NodeWithDryStruct.new(name: 'Name', items: [{name: 'A', value: 1.1}])
   }
 }
 
 puts 'Init NodeWithDryStruct with DryItem object as items'
 time_me.call -> {
-  10000.times {
+  NODE_CREATE_LIMIT.times {
     NodeWithDryStruct.new(name: 'Name', items: [DryItem.new(name: 'A', value: 1.1)])
+  }
+}
+
+puts 'Init NodeWithDryStruct with many hash as items'
+time_me.call -> {
+  NODE_WITH_MANY_ITEMS_CREATE_LIMIT.times {
+    NodeWithDryStruct.new(name: 'Name', items: hash_items)
   }
 }
