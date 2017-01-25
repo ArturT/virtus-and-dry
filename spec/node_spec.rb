5375235567fa4a4109076dc134b9a39e6bcd1bfc
ArturT <arturtrzop@gmail.com>
2017-01-25 12:45:30 +0100

    test missing value for Item

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/node'
 
 describe Node do
   let(:item_1) { Item.new(name: 'Item A', value: 1.1) }
-  let(:item_2) { Item.new(name: 'Item B', value: 2.2) }
+  let(:item_2) { Item.new(name: 'Item B') } # missing value
   let(:items) { [item_1, item_2] }
 
   let(:node) { Node.new(name: 'Node Name', items: items) }
@@ -18,5 +18,5 @@ describe Node do
   it { expect(node.items[1].name).to eq 'Item B' }
 
   it { expect(node.items[0].value).to eq 1.1 }
-  it { expect(node.items[1].value).to eq 2.2 }
+  it { expect(node.items[1].value).to be_nil }
 end
